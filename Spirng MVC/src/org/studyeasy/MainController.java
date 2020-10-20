c19ebc5e9fa6cb7ae3ea6890728e94f3b5189b83
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-20 12:42:21 -0300

    Change RequestMapping for GetMapping

--- Spirng MVC/src/org/studyeasy/MainController.java
+++ Spirng MVC/src/org/studyeasy/MainController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ public class MainController {
 		public String welcome() {
 			return "hello";
 		}
-		@RequestMapping(value = "displayname")
+		@GetMapping(value = "displayname")
 		public ModelAndView displayName(@RequestParam("firstName") String firstName) {
 			ModelAndView modelAndView = new ModelAndView("displayName");
 			modelAndView.addObject("name",firstName);
