2771f16c3d6860b52d784fc896d6fac89eb06301
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-20 10:11:57 -0300

    RequestParam and Model into the MainController

--- Spirng MVC/src/org/studyeasy/MainController.java
+++ Spirng MVC/src/org/studyeasy/MainController.java
@@ -3,8 +3,10 @@ package org.studyeasy;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -13,9 +15,8 @@ public class MainController {
 			return "hello";
 		}
 		@RequestMapping(value = "displayname")
-		public String displayName(HttpServletRequest request) {
-			String firstName=request.getParameter("firstName");
-			request.setAttribute("firstName", firstName);
+		public String displayName(@RequestParam("firstName") String firstName, Model model) {
+			model.addAttribute("name",firstName);
 			return "displayName";
 		}
 }
