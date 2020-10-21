eb65cd693ad6ebb216686987e0818043dcfd43e3
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-21 00:58:40 -0300

    User: Getters, Setters and toString

--- Spirng MVC/src/org/studyeasy/model/User.java
+++ Spirng MVC/src/org/studyeasy/model/User.java
@@ -1,5 +1,28 @@
 package org.studyeasy.model;
 
 public class User {
-	String name, gender;
+	private String name, gender;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", gender=" + gender + "]";
+	}
+	
+	
 }
