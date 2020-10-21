7183111cf3c613a8ab6c7501859881f50de5e93c
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-21 01:00:45 -0300

    Add user to the model and view

--- Spirng MVC/src/org/studyeasy/MainController.java
+++ Spirng MVC/src/org/studyeasy/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.studyeasy.model.User;
 
 
 @Controller
@@ -21,7 +22,8 @@ public class MainController {
 		@GetMapping("/")
 		public ModelAndView home() {
 			ModelAndView modelAndView=new ModelAndView("userFormView");
-			
+			User user=new User();
+			modelAndView.addObject("user",user);
 			return modelAndView;
 		}
 		@PostMapping("/displayUserInfo")
