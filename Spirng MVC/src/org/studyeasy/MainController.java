af0425243f4b2cca4c36ccaa8fb5669edd719623
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-21 00:46:03 -0300

    PostMapping logic to display user info

--- Spirng MVC/src/org/studyeasy/MainController.java
+++ Spirng MVC/src/org/studyeasy/MainController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,10 @@ public class MainController {
 			ModelAndView modelAndView=new ModelAndView("userFormView");
 			
 			return modelAndView;
+		}
+		@PostMapping("/displayUserInfo")
+		public ModelAndView displayUserInfo() {
+			return null;
 		}
 		
 }
