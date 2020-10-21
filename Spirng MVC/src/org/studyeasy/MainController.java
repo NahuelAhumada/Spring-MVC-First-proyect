b1bf86595ff05569c97d45b416ea2568385b14be
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-21 01:07:55 -0300

    Add model attribute into the Post mapping method

--- Spirng MVC/src/org/studyeasy/MainController.java
+++ Spirng MVC/src/org/studyeasy/MainController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,8 +28,11 @@ public class MainController {
 			return modelAndView;
 		}
 		@PostMapping("/displayUserInfo")
-		public ModelAndView displayUserInfo() {
-			return null;
+		public ModelAndView displayUserInfo(@ModelAttribute User user) {
+			ModelAndView modelAndView=new ModelAndView("displayUserInfo");
+			System.out.println(user);
+			modelAndView.addObject("user",user);
+			return modelAndView;
 		}
 		
 }
